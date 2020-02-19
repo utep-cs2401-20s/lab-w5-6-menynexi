2881167711a06a3d8cc85aae00b9804f71604159
Manuel Gutierrez <meny_nexi@outlook.com>
2020-02-19 14:46:33 -0700

    created basic constructors and elements to the file

---
+++ src/src/SnakeGame.java
@@ -0,0 +1,35 @@
+public class SnakeGame{
+    private boolean[][] game;
+    private int[] headPosition = new int[2];
+    private static int exhaustiveSearch;
+    private static int recursiveCheck;
+
+
+
+    //default constructor constructors
+    public SnakeGame(){
+        this.game = new boolean[1][1];
+        //this.headPosition;
+    }
+
+    //basic constructor with input of 2D array
+    public SnakeGame(boolean[][] givenBoard, int x, int y){
+        //copies the array
+        for(int i = 0; i < givenBoard.length; i++){
+            for(int j = 0; j < givenBoard[i].length; j++){
+                this.game[i][j] = givenBoard[i][j];
+            }
+        }
+        this.headPosition[0] = x;
+        this.headPosition[1] = y;
+    }
+
+
+
+
+
+
+    public static void main(String[] args){
+
+    }
+}
